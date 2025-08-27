3cd9bebee2b3c331fa1e31eb9ccd6460595f9d2e
sushandhakal125 <91354182+sushandhakal125@users.noreply.github.com>
2025-08-27 13:18:13 -0400

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,20 +2,20 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../config/keys");
-
 const opts = {};
+
+// ✅ Use Bearer token from Authorization header
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+
+// ✅ Load secret key from env
+opts.secretOrKey = process.env.JWT_SECRET;
 
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
         .then(user => {
-          if (user) {
-            return done(null, user);
-          }
+          if (user) return done(null, user);
           return done(null, false);
         })
         .catch(err => console.log(err));
